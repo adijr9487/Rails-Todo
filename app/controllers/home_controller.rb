40cb72837b94c3e7420754153674ac5256cbc4c3
Thrillophilia <adijr9487@gmail.com>
2023-03-13 10:55:28 +0530

    :hammer: added task

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     if session[:user_id].present?
       @user = User.find_by(id: session[:user_id])
+      @todos = Todo.where(user_id: session[:user_id])
     else
       redirect_to new_user_path
     end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,11 @@ Rails.application.routes.draw do
   post '/login', to: 'registration#login_create', as: :login_create_user
   post '/create', to: 'registration#create', as: :create_user
   delete '/logout', to: 'registration#logout', as: :logout_user
+
+  get '/new_todo', to: 'todo#new', as: :new_todo
+  post '/create_todo', to: 'todo#create', as: :create_todo
+  patch '/mark_complete/:id', to: 'todo#mark_complete', as: :mark_complete_todo
+  delete '/delete_todo/:id', to: 'todo#destroy', as: :delete_todo
+
   root to: 'home#index', as: :index_user
 end
